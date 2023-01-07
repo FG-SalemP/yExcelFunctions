b0ae0b631323334e39298d0f401012338d5593fb
salem <salem@LAPT-SFP-FTGSFC>
2023-01-07 07:53:36 -0700

    Add project files.

---
+++ yExcelFunctions/Functions.cs
@@ -0,0 +1,19 @@
+﻿using ExcelDna.Integration;
+
+namespace yExcelFunctions
+{
+    public class Functions
+    {
+        [ExcelFunction(Description = "For Voyager Multi-Select: Combines values together with a carat ^")]
+        public static string yCarat(object[] list)
+        {
+            return String.Join("^", list);
+        }
+
+        [ExcelFunction(Description = "For SQL: Combines values together in SQL formatted Strings")]
+        public static string yIn(object[] list)
+        {
+            return $"'{String.Join("','", list)}'";
+        }
+    }
+}
